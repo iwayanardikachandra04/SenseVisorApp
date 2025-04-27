51fb083361b2ac53a16211a6422aabe932191345
Wayan Chandra <wayan.chandra@gmail.com>
2025-04-27 23:05:50 +0700

    add more layout

--- app/src/main/java/com/example/sensevisor/ui/DetailActivity.kt
+++ app/src/main/java/com/example/sensevisor/ui/DetailActivity.kt
@@ -3,17 +3,17 @@ package com.example.sensevisor.ui
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import com.example.sensevisor.databinding.ActivityHistoryBinding
+import com.example.sensevisor.databinding.ActivityDetailBinding
 
-class HistoryActivity : AppCompatActivity() {
+class DetailActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityHistoryBinding
+    private lateinit var binding: ActivityDetailBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
 
-        binding = ActivityHistoryBinding.inflate(layoutInflater)
+        binding = ActivityDetailBinding.inflate(layoutInflater)
         setContentView(binding.root)
     }
 }
